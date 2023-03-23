46bd373cfec200831f00b48acb99c0d879d5bb54
raj <raj.rajeev2244@gmail.com>
2023-03-23 21:03:30 +0530

    Added New Line

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
 	public static void main(String[] args){
 	System.out.println("Welcome User");
 	System.out.println(java.time.LocalDateTime.now());
+	System.out.println("Welcome to GIT");
 }
 }
